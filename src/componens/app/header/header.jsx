de615c66ca6bedad52c5b920639af808819b291a
Alexsey779 <fktrctq321@gmail.com>
2025-09-28 18:51:57 +0700

    text

--- src/componens/app/header/header.jsx
+++ src/componens/app/header/header.jsx
@@ -3,6 +3,7 @@ function Header() {
         <header className="container">
             <h1>Заголовок</h1>
             <p>hello?</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum at, nisi labore illo unde molestiae quia libero minima aut ex rem impedit consectetur aperiam distinctio, expedita doloribus blanditiis consequatur obcaecati?</p>
         </header>
     )
 }
